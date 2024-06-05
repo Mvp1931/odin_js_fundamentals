92eb4b9fc83f88b6f58711d19c60f7f05be67205
Mvp1931 <mihirphadnis001@gmail.com>
2024-06-05 13:14:07 +0530

    modified first js file

--- JS-01/javascripts/main.js
+++ JS-01/javascripts/main.js
@@ -1,1 +1,36 @@
 console.log("Hello from JS-01");
+
+console.log(23 + 97);
+
+let number1 = 23;
+let number2 = 97;
+let number3 = number1 + number2;
+let number4 = number1 - number2;
+let number5 = number1 * number2;
+let number6 = number1 / number2;
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(number5);
+console.log(number6);
+
+console.log(number1 + number2 + number3 + number4 + number5 + number6);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(9 * a);
+
+let b = 7 * a;
+
+console.log(b);
+
+
+const MAX = 57;
+
+const actual = MAX - 13;
+
+const percentage = actual / MAX;
